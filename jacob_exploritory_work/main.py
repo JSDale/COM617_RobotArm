6e4620d7dd1e4fb6370444400a784c28dfe01700
Jacob.Dale <jacob.dale@qinetiq.com>
2022-03-10 14:45:38 +0000

    created the beginings of an abstraction layer for the robot arm

--- jacob_exploritory_work/main.py
+++ jacob_exploritory_work/main.py
@@ -7,6 +7,10 @@ def main():
     arm.move(usb_arm.LedOn)
     arm.move(usb_arm.BaseClockWise)
     time.sleep(1)
+    arm.move(usb_arm.CloseGrips)
+    arm.move(usb_arm.GripsClose)
+    arm.move(usb_arm.ElbowDown)
+    arm.move(usb_arm.ElbowUp)
     arm.move(usb_arm.Stop)
 
 
