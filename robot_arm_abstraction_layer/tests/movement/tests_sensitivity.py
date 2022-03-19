9835d164c2d7504f5ad8b842cf3f9765e9c7ae49
Jacob.Dale <jacob.dale@qinetiq.com>
2022-03-19 13:25:30 +0000

    added unit test to test functionality of setting a value

---
+++ robot_arm_abstraction_layer/tests/movement/tests_sensitivity.py
@@ -0,0 +1,16 @@
+import unittest
+
+from maplin_arm_controller import MaplinArmController
+from message_handlers import move_wrist, set_sensitivity
+
+
+class TestsSensitivity(unittest.TestCase):
+
+    def tests_can_set(self):
+        controller = MaplinArmController('0.1')
+        wrist = move_wrist.MoveWrist(controller)
+        self.assertTrue(wrist.controller.sensitivity == '0.1')
+        print(f"value: {wrist.controller.sensitivity == '0.1'}")
+
+        controller.set_sensitivity('5')
+        self.assertTrue(wrist.controller.sensitivity == '5')
